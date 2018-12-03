3d499e4e44634ff9e7839b7153397ae626ebcaac
Pedro Sampaio <pedro.sampaio@faro.com>
2018-12-03 15:06:18 +0000

    Added day 1.1

---
+++ 1.1/AoC_01_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC_01_1
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Loading input");
+
+            var fileName = args[0];
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"Input file not found (${fileName})");
+            }
+            else
+            {
+                var result = (await File.ReadAllLinesAsync(args[0])).Sum(int.Parse);
+                Console.WriteLine($"Resulting frequency is {result}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
