4f539de891b22b88014c3d84b768232e16dab212
Pedro Sampaio <sampaio.pedro@gmail.com>
2018-12-04 22:38:19 +0000

    Added day 4, part 2

--- src/Day04.cs
+++ src/Day04.cs
@@ -37,17 +37,70 @@ namespace AdventOfCode
 
             Array.Sort(input, StringComparer.InvariantCulture);
 
+            var guards = GetGuardSummary(input);
+            var guard = guards.Values.OrderByDescending(g => g.Sleep.Sum()).First();
+            int maxSleepMinute = CalculateMaxSleepMinute(guard);
+            return guard.Id * maxSleepMinute;
+        }
+
+        public object RunPart2(string[] input)
+        {
+//            input = new[]
+//            {
+//                "[1518-11-01 00:00] Guard #10 begins shift",
+//                "[1518-11-01 00:05] falls asleep",
+//                "[1518-11-01 00:25] wakes up",
+//                "[1518-11-01 00:30] falls asleep",
+//                "[1518-11-01 00:55] wakes up",
+//                "[1518-11-01 23:58] Guard #99 begins shift",
+//                "[1518-11-02 00:40] falls asleep",
+//                "[1518-11-02 00:50] wakes up",
+//                "[1518-11-03 00:05] Guard #10 begins shift",
+//                "[1518-11-03 00:24] falls asleep",
+//                "[1518-11-03 00:29] wakes up",
+//                "[1518-11-04 00:02] Guard #99 begins shift",
+//                "[1518-11-04 00:36] falls asleep",
+//                "[1518-11-04 00:46] wakes up",
+//                "[1518-11-05 00:03] Guard #99 begins shift",
+//                "[1518-11-05 00:45] falls asleep",
+//                "[1518-11-05 00:55] wakes up"
+//            };
+
+            Array.Sort(input, StringComparer.InvariantCulture);
+
+            var guards = GetGuardSummary(input);
+            var guard = guards.Values.OrderByDescending(g => g.Sleep.Max()).First();
+            int maxSleepMinute = CalculateMaxSleepMinute(guard);
+
+            return guard.Id * maxSleepMinute;
+        }
+
+        private static int CalculateMaxSleepMinute(Guard guard)
+        {
+            int maxSleep = 0;
+            int maxSleepMinute = -1;
+            for (int i = 0; i < guard.Sleep.Length; i++)
+            {
+                if (guard.Sleep[i] <= maxSleep)
+                {
+                    continue;
+                }
+
+                maxSleep = guard.Sleep[i];
+                maxSleepMinute = i;
+            }
+
+            return maxSleepMinute;
+        }
+
+        private Dictionary<int, Guard> GetGuardSummary(IEnumerable<string> input)
+        {
             var guards = new Dictionary<int, Guard>();
             Guard guard = null;
 
             foreach (string line in input)
             {
                 var timeMatch = timePattern.Match(line);
-
-//                var dateTime = new DateTime(
-//                    int.Parse(timeMatch.Groups[1].Value),
-//                    int.Parse(timeMatch.Groups[2].Value),
-//                    int.Parse(timeMatch.Groups[3].Value));
 
                 int hour = int.Parse(timeMatch.Groups[5].Value);
                 string action = timeMatch.Groups[6].Value;
@@ -85,28 +138,9 @@ namespace AdventOfCode
                 }
             }
 
-            guard = guards.Values.OrderByDescending(g => g.Sleep.Sum()).First();
-            
-            int maxSleep = 0;
-            int maxSleepHour = -1;
-            for (int i = 0; i < guard.Sleep.Length; i++)
-            {
-                if (guard.Sleep[i] <= maxSleep)
-                {
-                    continue;
-                }
-
-                maxSleep = guard.Sleep[i];
-                maxSleepHour = i;
-            }
-
-            return guard.Id * maxSleepHour;
+            return guards;
         }
 
-        public object RunPart2(string[] input)
-        {
-            return null;
-        }
 
         private class Guard
         {
