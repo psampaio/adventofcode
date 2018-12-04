0496c687af461bce244ac5ec7e8e3b8fb8f9829e
Pedro Sampaio <pedro.sampaio@faro.com>
2018-12-04 12:04:17 +0000

    Added day 2, part 1

---
+++ src/Day02.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+// ReSharper disable StringLiteralTypo
+
+namespace AdventOfCode
+{
+    public class Day02 : IPuzzle
+    {
+        public int Day => 2;
+
+        public async Task<int> RunPart1(string input)
+        {
+            var lines = await File.ReadAllLinesAsync(input);
+            //var lines = new[]
+            //{
+            //    "abcdef",
+            //    "bababc",
+            //    "abbcde",
+            //    "abcccd",
+            //    "aabcdd",
+            //    "abcdee",
+            //    "ababab",
+            //};
+
+            var appearsTwoTimes = 0;
+            var appearsThreeTimes = 0;
+
+            foreach (var line in lines)
+            {
+                var letterCountPerLine = new int[26];
+                foreach (var c in line)
+                {
+                    letterCountPerLine[c - 97]++;
+                }
+
+                var incrementTwoCount = false;
+                var incrementThreeCount = false;
+                foreach (var c in letterCountPerLine)
+                {
+                    if (c == 2)
+                    {
+                        incrementTwoCount = true;
+                    }
+
+                    if (c == 3)
+                    {
+                        incrementThreeCount = true;
+                    }
+                }
+
+                if (incrementTwoCount)
+                {
+                    appearsTwoTimes++;
+                }
+
+                if (incrementThreeCount)
+                {
+                    appearsThreeTimes++;
+                }
+            }
+
+            return appearsTwoTimes * appearsThreeTimes;
+        }
+
+        public Task<int> RunPart2(string input)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
--- src/IPuzzle.cs
+++ src/IPuzzle.cs
@@ -4,8 +4,8 @@ namespace AdventOfCode
 {
     public interface IPuzzle
     {
+        int Day { get; }
         Task<int> RunPart1(string input);
         Task<int> RunPart2(string input);
-        int Day { get; }
     }
 }
